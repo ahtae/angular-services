5583a11874b80482ab3f56e1a7f0c1245e55b5bd
Kristy Li <kristyli20.0@gmail.com>
2021-05-04 20:45:57 -0400

    feat: Create a BookTrackerErrorHandlerService

--- src/app/core/book-tracker-error-handler.service.ts
+++ src/app/core/book-tracker-error-handler.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { ErrorHandler, Injectable } from '@angular/core';
+import { BookTrackerError } from 'app/models/bookTrackerError';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BookTrackerErrorHandlerService {
+export class BookTrackerErrorHandlerService implements ErrorHandler {
 
   constructor() { }
+  handleError(error: any): void {
+    let customError: BookTrackerError = new BookTrackerError();
+    customError.errorNumber = 200;
+    customError.message = (<Error>error).message;
+    customError.friendlyMessage = 'An error occurred. Please try again';
+
+    console.log(customError);
+  }
 }
